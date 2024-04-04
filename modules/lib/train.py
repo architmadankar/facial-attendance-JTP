75d1902fd2e593294286f5ac18287717abffe83b
Archit Madankar <architmadankar21@gmail.com>
2024-04-04 20:14:20 +0530

    Fix: Training Classification error on CLI

--- modules/lib/train.py
+++ modules/lib/train.py
@@ -13,18 +13,26 @@ class ClassificationTrain:
             print("Checking Encodings [INFO]")
             with open(FILE_ENCODING, "rb") as x: #file path
                 data = pickle.loads(x.read())
-                #init recognized encodings and names
                 rec_encodings = data["encodings"]
                 rec_ids = data["ids"]
         except FileNotFoundError:
             rec_encodings = []
             rec_ids = []
-            #converting _id into int and set
+
         unq_id =  [int(_id) for _id in set(rec_ids)]
-        path_id = [os.path.join(PATH_DATASET, f) for f in os.listdir(PATH_DATASET)]
-        
+
+        try:
+            path_id = [os.path.join(PATH_DATASET, f) for f in os.listdir(PATH_DATASET)]
+        except FileNotFoundError:
+            print(f"[ERROR] The directory {PATH_DATASET} does not exist.")
+            return
+
+        if not path_id:
+            print(f"[INFO] No images found in {PATH_DATASET}.")
+            return
+
         for path_of_id in path_id:
-            _id = int(os.path.split(path_id)[1])
+            _id = int(os.path.split(path_of_id)[1])  # corrected variable name
             if _id in unq_id:
                 continue
             path_img = [os.path.join(path_of_id, f) for f in os.listdir(path_of_id)]
@@ -34,23 +42,19 @@ class ClassificationTrain:
                 try:
                     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 except cv2.error:
-                    
                     try:
                         os.remove(path_of_img)
                     except (FileNotFoundError, PermissionError):
                         pass
                     continue
-                #detect coords of box
+
                 squares = face_recognition.face_locations(rgb, model=MODEL_DLIB)
                 encs = face_recognition.face_encodings(rgb, squares)
                 for enc in encs:
                     rec_encodings.append(enc)
                     rec_ids.append(_id)
-                    
+
         print("Encoding_Serialize")
         data = {"encs": rec_encodings, "ID": rec_ids}
-        f=open(FILE_ENCODING, "wb")
-        f.write(pickle.dumps(data))
-        f.close()
-                    
-                    
+        with open(FILE_ENCODING, "wb") as f:  # use context manager for file writing
+            f.write(pickle.dumps(data))
