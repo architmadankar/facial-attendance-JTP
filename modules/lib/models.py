7eb56152c13e8f37d2acd42e55f0ac4be68d6bc0
Archit Madankar <architmadankar21@gmail.com>
2024-04-04 13:28:23 +0530

    define admin model

--- modules/lib/models.py
+++ modules/lib/models.py
@@ -3,9 +3,30 @@ from modules.db import Session
 
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column, Boolean
+
 DBase=declarative_base()
 
 
 class Admin(DBase):
     __tablename__ = "admins"
+    id = Column(Integer, primary_key=True)
+    user= Column(String(30), nullable=False, unique=True)
+    passwd= Column(String(30), nullable=False)
     
+    @classmethod
+    def get_by_id(cls, _id: int) -> "Admin":
+        return Session.query(cls). filter_by(id=id).first()
+    
+    @classmethod
+    def get_by_user(cls, user: str) -> "Admin":
+        return Session.query(cls). filter_by(user=user).first()
+    
+    def create(self) -> None:
+        Session.add(self)
+        Session.commit()
+    
+    def remove(self) -> None:
+        Session.delete(self)
+        Session.commit()
+        
