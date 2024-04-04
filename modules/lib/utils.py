edd84458a244dbd79fba1f4cd036bd8cd2f1bdab
Archit Madankar <architmadankar21@gmail.com>
2024-04-04 14:29:10 +0530

    Update utils.py

--- modules/lib/utils.py
+++ modules/lib/utils.py
@@ -6,10 +6,14 @@ import pickle
 from typing import Union
 from datetime import date as d
 
+from modules.settings import PATH_DATASET
 from modules.models import User
+
+import os
 
 class AppUtils:
     app_title = "Facial Attendance"
+    
     def __init__(self, vid_input: Union[int, str]):
         self.vid_input = vid_input
     
@@ -27,8 +31,16 @@ class AppUtils:
         x.release()
         cv2.destroyAllWindows()
         
-        @classmethod
-        def create_n_write(cls, name:str) -> str:
-            user = User(name=name)
-            user.create()
-            
+    @classmethod
+    def create_n_write(cls, name:str) -> str:
+        user = User(name=name)
+        user.create()
+        path_id= f"{PATH_DATASET}{os.sep}{user.id}"
+        if not os.path.exists(path_id):
+            os.makedirs(path_id)
+        return path_id
+        
+    def check_n_snap(self):
+        name = input("Enter User's Name: ")
+        path_id = self.create_n_write(name)
+        x = cv2.VideoCapture(self.vid_input)    
