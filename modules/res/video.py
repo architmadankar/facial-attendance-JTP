25ae3ae39488c7e28ba3bb3e21ce3f06989410f4
Archit Madankar <architmadankar21@gmail.com>
2024-04-08 23:33:18 +0530

    initialize frontend

--- modules/res/video.py
+++ modules/res/video.py
@@ -42,3 +42,4 @@ class VideoActive(Resource):
                     b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
             )  
+#TODO video start stop and delete
