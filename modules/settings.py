9773612963df34f02e6353cd42f90fc0b4e023ba
Archit Madankar <architmadankar21@gmail.com>
2024-04-09 13:32:28 +0530

    small integration fixes

--- modules/settings.py
+++ modules/settings.py
@@ -4,6 +4,8 @@ from decouple import config
 class AppConfig:
     DEBUG = config('DEBUG', default=True, cast=bool)
     PROPAGATE_EXCEPTIONS = True
+    UPLOADED_IMAGES_DEST = os.path.join("static", "images")
+
 
 #lib/train.py
 FILE_ENCODING = os.path.join("classifier", "encodings.pickle")
@@ -14,3 +16,4 @@ DLIB_TOLERANCE = 0.6  # 0.6 -> default, 0.72 -> aggressive
 HAAR_CASCADE_CLASSIFIER_PATH = os.path.join("classifier", "haarcascade_frontalface_default.xml")
 
 CAMERA_SOURCE = config('OPENCV_VIDEO', default=0, cast=int)
+UNKNOWN_IMAGES_PATH = os.path.join("static", "images", "unknown")
