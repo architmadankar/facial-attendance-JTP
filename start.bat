3b04e627702571a9b5d5b2bfdb4c635608c9b4d7
Archit Madankar <architmadankar21@gmail.com>
2024-04-16 18:51:43 +0530

    Update start.bat add conda functions

--- start.bat
+++ start.bat
@@ -1,7 +1,9 @@
 @echo off
 cd backend
-call python -m venv env
-call venv\Scripts\activate
+::call python -m venv env
+::call venv\Scripts\activate
+call conda create --name env
+call conda activate env
 call pip install -r requirements.txt
 start cmd /k "python api.py"
 
